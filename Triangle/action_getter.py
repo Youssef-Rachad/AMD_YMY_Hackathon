18037032fb6b25b5e3f243070589e9a9b72d4e51
Matthew Oliveira <67080763+hdmd2003@users.noreply.github.com>
2024-06-13 10:56:18 -0400

    Update action_getter.py

--- Triangle/action_getter.py
+++ Triangle/action_getter.py
@@ -1,16 +1,28 @@
 import json
 
-
-def read_sarif_file(file_path):
+def parse_sarif(file_path):
     with open(file_path, 'r') as file:
         data = json.load(file)
-    return data
 
-sarif_data = read_sarif_file('../SARIF_DEPOT/.sarif')
+    alerts = []
+    for run in data['runs']:
+        for result in run['results']:
+            alert = {}
+            alert['ruleId'] = result['ruleId']
+            alert['message'] = result['message']['text']
+            #alert['level'] = result['level']
+            if 'locations' in result:
+                alert['locations'] = []
+                for location in result['locations']:
+                    loc = {}
+                    loc['uri'] = location['physicalLocation']['artifactLocation']['uri']
+                    if 'region' in location['physicalLocation']:
+                        loc['startLine'] = location['physicalLocation']['region']['startLine']
+                        #loc['startColumn'] = location['physicalLocation']['region']['startColumn']
+                    alert['locations'].append(loc)
+            alerts.append(alert)
+    return alerts
 
-alerts = []
-for run in sarif_data["runs"]:
-    for result in run["results"]:
-        alerts.append(result["message"]["text"])
-
-print(alerts)
+alerts = parse_sarif('../2024-06-12/python.sarif')
+for alert in alerts:
+    print(alert)
